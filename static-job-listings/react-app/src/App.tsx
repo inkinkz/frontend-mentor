09bf0acc38f3e3e42456a05dc5f4d9c631e72cef
inkinkz <nithiphorn.n@gmail.com>
2020-03-31 18:53:05 +0700

    job-listing search bar UI

--- static-job-listings/react-app/src/App.tsx
+++ static-job-listings/react-app/src/App.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="banner">
+        <div className="search-bar">
+          <div className="search-items-container">
+            <div className="search-item">
+              <div className="search-text">Frontend</div>
+              <div className="remove-button">
+                <FontAwesomeIcon icon={faTimes} />
+              </div>
+            </div>
+            <div className="search-item">
+              <div className="search-text">CSS</div>
+              <div className="remove-button">
+                <FontAwesomeIcon icon={faTimes} />
+              </div>
+            </div>
+          </div>
+
+          <div className="clear-button">
+            <div className="clear-text">clear</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
