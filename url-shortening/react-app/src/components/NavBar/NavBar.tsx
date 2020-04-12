5d7fbba078981f61c3d20c6d83aa1278726dc475
inkinkz <nithiphorn.n@gmail.com>
2020-04-12 20:43:15 +0700

    Icon resized

--- url-shortening/react-app/src/components/NavBar/NavBar.tsx
+++ url-shortening/react-app/src/components/NavBar/NavBar.tsx
@@ -36,7 +36,7 @@ const NavBar = () => {
         </ul>
         {/* Hamburger Button */}
         <div className="menu" onClick={() => toggleMenu()}>
-          <img src={Menu} alt="menu" width="40px" height="40px" />
+          <img src={Menu} alt="menu" width="32px" height="32px" />
         </div>
       </nav>
       {/* Menu List */}
